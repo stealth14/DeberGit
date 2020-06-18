8cf4ead97e5f1e52c4aba43cec0dc21350cad900
Ronny Cajas <ronny.cajas@epn.edu.ec>
2020-06-18 11:36:01 -0500

    project setup

---
+++ src/main/java/Modelo/ClsMenu.java
@@ -0,0 +1,31 @@
+package Modelo;
+import Modelo.ClsPersona;
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ronnycajas
+ */
+public class ClsMenu {
+        public static void main(String[] args) {
+        
+        ClsPersona persona = new ClsPersona("Ronny");
+        
+        System.out.println(persona.getName());
+
+        persona.setName("Kevin");
+    
+        System.out.println(persona.getName());
+
+        
+    }
+}
+
+
+
---
+++ src/main/java/Modelo/ClsPersona.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+/**
+ *
+ * @author ronnycajas
+ */
+public class ClsPersona {
+    
+    private String name;
+    private String age;
+
+    
+    public ClsPersona(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+}
+
